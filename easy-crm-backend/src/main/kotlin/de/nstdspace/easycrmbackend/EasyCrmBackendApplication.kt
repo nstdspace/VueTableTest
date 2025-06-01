ee1fadd4ea57e7f4c7f16b2d42d01adacdaa5fbf
Maksim Rotmann <maksim.rotmann@l21s.de>
2025-06-01 15:06:52 +0200

    Add spring backend

---
+++ easy-crm-backend/src/main/kotlin/de/nstdspace/easycrmbackend/EasyCrmBackendApplication.kt
@@ -0,0 +1,11 @@
+package de.nstdspace.easycrmbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasyCrmBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<EasyCrmBackendApplication>(*args)
+}
---
+++ easy-crm-backend/src/test/kotlin/de/nstdspace/easycrmbackend/EasyCrmBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.nstdspace.easycrmbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EasyCrmBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
