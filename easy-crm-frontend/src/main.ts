a4ffa369c285277bfc52504e972eec99c5155831
Maksim Rotmann <maksim.rotmann@gmail.com>
2025-06-06 18:12:20 +0200

    Add configs

--- easy-crm-frontend/src/main.ts
+++ easy-crm-frontend/src/main.ts
@@ -2,13 +2,24 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueKeycloak from '@dsb-norge/vue-keycloak-js'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(VueKeycloak, {
+  config: {
+    url: 'http://localhost:8180',
+    realm: 'easy-crm',
+    clientId: 'easy-crm-frontend',
+  },
+  init: {
+    onLoad: 'login-required',
+    checkLoginIframe: false,
+  },
+  onReady: () => app.mount('#app'),
+})
 app.use(createPinia())
 app.use(router)
-
-app.mount('#app')
